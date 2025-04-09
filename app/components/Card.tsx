884ccb3613b34dfe1397fa9cfc0127ceed81e194
Ryaaan4321 <aryan056400@gmail.com>
2025-04-09 17:51:58 +0530

    responsivenessss

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -3,74 +3,76 @@ import jobinterface from "../lib/actions";
 
 export default function Card({ job }: { job: jobinterface[] }) {
     return (
-        <div className="grid grid-cols-4 gap-1 card">
-            {job.map((item, index) => (
-                <div
-                    key={index}
-                    className="max-w-sm rounded-2xl overflow-hidden border border-gray-200 shadow-md hover:shadow-lg transition-shadow duration-300 mb-4"
-                >
-                    <div className="flex items-center gap-3 p-4">
-                        <div className="w-10 h-10 bg-black text-white flex items-center justify-center rounded-lg text-sm font-semibold">
-                            {item.title?.[0] || "H"}
-                        </div>
-                        <div>
-                            <div className="text-sm font-semibold text-gray-900">
-                                {item.title}
+        <div>
+            <div className="grid grid-cols-4 gap-1 card">
+                {job.map((item, index) => (
+                    <div
+                        key={index}
+                        className="max-w-sm rounded-2xl overflow-hidden border border-gray-200 shadow-md hover:shadow-lg transition-shadow duration-300 mb-4"
+                    >
+                        <div className="flex items-center gap-3 p-4">
+                            <div className="w-10 h-10 bg-black text-white flex items-center justify-center rounded-lg text-sm font-semibold">
+                                {item.title?.[0] || "H"}
                             </div>
-                            <div className="text-xs text-gray-500">
-                                {item.descreption}
+                            <div>
+                                <div className="text-sm font-semibold text-gray-900">
+                                    {item.title}
+                                </div>
+                                <div className="text-xs text-gray-500">
+                                    {item.descreption}
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div className="border-t px-4 py-3 text-sm text-gray-700">
-                        <div className="flex space-x-1 items-center">
-                            <div className="text-zinc-800 font-semibold text-xl">Company Name :</div>
-                            <div>{item.companyname}</div>
-                        </div>
-                        <div className="flex space-x-1 items-center">
-                            <div className="text-zinc-800 font-semibold text-xl">
-                                Posted By:
+                        <div className="border-t px-4 py-3 text-sm text-gray-700">
+                            <div className="flex space-x-1 items-center">
+                                <div className="text-zinc-800 font-semibold text-xl">Company Name :</div>
+                                <div>{item.companyname}</div>
                             </div>
-                            <div className="text-xl font-medium font-gray-600">{item.postedby.name}</div>
+                            <div className="flex space-x-1 items-center">
+                                <div className="text-zinc-800 font-semibold text-xl">
+                                    Posted By:
+                                </div>
+                                <div className="text-xl font-medium font-gray-600">{item.postedby.name}</div>
+                            </div>
                         </div>
-                    </div>
-                    <div className="border-t px-4 py-3 text-sm text-gray-700 flex flex-wrap gap-2">
-                        {item.jobTypes.map((type, index) => (
-                            <span
-                                key={index}
-                                className="bg-gray-100 text-gray-800 px-2 py-1 rounded-full text-xs font-medium"
-                            ><div> {type.name}</div>
+                        <div className="border-t px-4 py-3 text-sm text-gray-700 flex flex-wrap gap-2">
+                            {item.jobTypes.map((type, index) => (
+                                <span
+                                    key={index}
+                                    className="bg-gray-100 text-gray-800 px-2 py-1 rounded-full text-xs font-medium"
+                                ><div> {type.name}</div>
 
-                            </span>
-                        ))}
-                    </div>
-                    <div className="border-t px-4 py-3">
-                        <a
-                            href={item.joblink || "https://github.com"}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="text-sm text-blue-600 hover:underline inline-flex items-center"
-                        >
-                            Visit the Link
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="w-4 h-4 ml-1"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
+                                </span>
+                            ))}
+                        </div>
+                        <div className="border-t px-4 py-3">
+                            <a
+                                href={item.joblink || "https://github.com"}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-sm text-blue-600 hover:underline inline-flex items-center"
                             >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M14 3h7m0 0v7m0-7L10 14"
-                                />
-                            </svg>
-                        </a>
+                                Visit the Link
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="w-4 h-4 ml-1"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth={2}
+                                        d="M14 3h7m0 0v7m0-7L10 14"
+                                    />
+                                </svg>
+                            </a>
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     );
 }
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -25,37 +25,37 @@ export default function Header() {
           href="/dashboard"
           className={`transition-colors text-slate-600 text-3xl font-light`}
         >
-          Overview
+          {/* Overview */}
         </Link>
         <Link
           href="/customers"
           className={`transition-colors text-slate-600 text-3xl font-light`}
         >
-          Users
+          {/* Users */}
         </Link>
         <Link
           href="/products"
           className={`transition-colors text-slate-600 text-3xl font-light`}
         >
-          Products
+          {/* Products */}
         </Link>
         <Link
           href="/settings"
           className={`transition-colors text-slate-600 text-3xl font-light`}
         >
-          Settings
+          {/* Settings */}
         </Link>
       </nav>
 
       {/* Right section - Search */}
-      <div className="relative border-black hideelm">
-        <input
-          type="text"
-          placeholder="Search..."
-          className="pl-3 pr-8 py-2 text-sm border border-black rounded-md focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-gray-400 w-48"
-        />
+      {/* <div className="relative border-black hideelm"> */}
+        {/* <input */}
+          {/* // type="text" */}
+          {/* // placeholder="Search..." */}
+          {/* // className="pl-3 pr-8 py-2 text-sm border border-black rounded-md focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-gray-400 w-48" */}
+        {/* // > */}
         {/* <MagnifyingGlassIcon className="h-4 w-4 text-gray-400 absolute right-2.5 top-2.5" /> */}
-      </div>
+      {/* </div> */}
       <div className='flex ml-11 hidehamburger'>
         <svg width="70" height="70" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 4.5C2 4.22386 2.22386 4 2.5 4H12.5C12.7761 4 13 4.22386 13 4.5C13 4.77614 12.7761 5 12.5 5H2.5C2.22386 5 2 4.77614 2 4.5ZM7 7.5C7 7.22386 7.22386 7 7.5 7H12.5C12.7761 7 13 7.22386 13 7.5C13 7.77614 12.7761 8 12.5 8H7.5C7.22386 8 7 7.77614 7 7.5ZM4 10.5C4 10.2239 4.22386 10 4.5 10H12.5C12.7761 10 13 10.2239 13 10.5C13 10.7761 12.7761 11 12.5 11H4.5C4.22386 11 4 10.7761 4 10.5Z" fill="currentColor"></path></svg>
       </div>
