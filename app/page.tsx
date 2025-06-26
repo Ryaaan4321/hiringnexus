85e3f87c045fe4ee8266e765f91640652e8e0afd
Ryaaan4321 <aryan056400@gmail.com>
2025-06-26 13:18:44 +0530

    feat: update Home component layout and styling; add ContainerTextFlip component for animated text

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,25 @@
 "use client"
 import { Asterisk } from "lucide-react";
+import Link from "next/link";
+
 export default function Home() {
-
   return (
-    <>
-      <div className="flex justify-center items-center h-screen ">
-        <div className="flex flex-col  items-center text-7xl font-semibold text-black">
-          {/* <Asterisk className="h-10 w-10" /> */}
-          <div>HiringNexus</div>
-          <div className="text-3xl">
-            One Stop for the HR and the Students
+    <div className="flex justify-center items-center min-h-screen bg-white px-4">
+      <div className="flex flex-col items-center font-semibold text-black w-full max-w-xl">
+        <div className="flex flex-wrap items-center justify-center w-full space-y-2">
+          <div className="font-semibold text-gray-700 text-lg sm:text-xl md:text-4xl lg:text-4xl">
+            One Stop for the HR and the <span className="bg-slate-800 text-white p-1 text-3xl">Students </span>
           </div>
-        </div>
-        <div>
-
+          <div className="text-5xl sm:text-6xl md:text-8xl lg:text-9xl">
+            HiringNexus
+          </div>
+          <Link href="/auth/user/signin" className="w-auto">
+            <div className="text-lg sm:text-xl md:text-2xl text-gray-700 ml-4">
+              Start your Journey <span className="bg-slate-900 text-white p-1 ">Now</span>
+            </div>
+          </Link>
         </div>
       </div>
-    </>
+    </div>
   );
 }
-
-
-/* color options that i have 
-Neutral
-Gray
-Zinc
-Stone
-Slate
-*/
