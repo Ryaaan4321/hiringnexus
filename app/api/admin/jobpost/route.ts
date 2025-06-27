f83c9591add639a39473e0cfa7194b14ba603ebd
Ryaaan4321 <aryan056400@gmail.com>
2025-06-27 17:06:13 +0530

    refactor: simplify POST function in job post route; improve code readability and formatting

--- app/api/admin/jobpost/route.ts
+++ app/api/admin/jobpost/route.ts
@@ -6,23 +6,20 @@ interface AdminPayload extends JWTPayload {
     id: string;  // to extract the admin id from here 
     email: string;
 }
-
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextRequest) {
     try {
         const token = req.cookies.get("token")?.value;
         if (!token) {
             return NextResponse.json({ msg: "token  is missing" }, {
                 status: 401
-            })
-        };
+            });
+        }
         const { payload } = await jwtVerify<AdminPayload>(token, new TextEncoder().encode(process.env.SECRET_KEY));
         const adminId = payload.id;
         const body = await req.json();
         const admin = await client.admin.findUnique({
-            where: {
-                id: adminId
-            }
-        })
+            where: { id: adminId }
+        });
         if (!admin) {
             return NextResponse.json({ msg: "admin not found from the token" }, { status: 404 });
         }
@@ -32,23 +29,20 @@ export async function POST(req: NextRequest, res: NextResponse) {
                 descreption: body.descreption,
                 joblink: body.joblink,
                 postedby: { connect: { id: admin.id } },
-                jobTypes:body.jobTypes ,
-                experience:body.experience,
-                salary:body.salary,
-                companyname:body.companyname
+                jobTypes: body.jobTypes,
+                experience: body.experience,
+                salary: body.salary,
+                companyname: body.companyname
             },
             include: {
                 postedby: {
-                    select:{
-                        name:true
-                    }
-                },
-                
+                    select: { name: true }
+                }
             }
         });
-        return NextResponse.json({ newJob }, { status: 201 })
+        return NextResponse.json({ newJob }, { status: 201 });
     } catch (e: any) {
-        console.log("error message = ",e.message);
+        console.log("error message = ", e.message);
         return NextResponse.json({ msg: e.message }, { status: 500 });
     }
 }
